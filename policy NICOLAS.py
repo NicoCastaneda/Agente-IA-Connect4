9680e02125103e82f656b2dd9a93574f304db170
Nicolás Castañeda <nicolascasol@unisabana.edu.co>
2025-11-22 22:19:39 -0500

    version 2 NICOLAS
    ya demuestra aprendizaje, pero empata demasiadas veces

--- policy NICOLAS.py
+++ policy NICOLAS.py
@@ -9,60 +9,21 @@ from connect4.connect_state import ConnectState
 
 class NICOLAS(Policy):
 
-    def serializar(self, tablero): #es para convertir el tablero en una cadena unica
+    def serializar(self, tablero): #es para convertir el tablero en cadena unica de tuplas para usarla como key del diccionario
         return str(tuple(map(tuple, tablero)))
     
-    def acciones_legales(self, tablero): #si una oclumna esta llena 
-        col_validas= []
+
+    def acciones_legales(self, tablero): #columnas validas
+        col_validas=[]
 
         for col in range(7):
-            if tablero[0, col]== 0: #si la fila de arriba esta vacia
+            if tablero[0, col]==0: #si al parte de arriba esta libre (fila 0, columna x)
                 col_validas.append(col)
+
         return col_validas
     
 
-    def estado_raiz(self):
-        tablero_vacio = np.zeros((6,7), dtype=int)
-        s0= self.serializar(tablero_vacio)
-
-        self.Q_global[s0]= {} 
-        self.N_global[s0]= {}
-
-        #elejir el json segun el color
-        if self.mi_color==-1:
-            json=self.mejores_rojo["0"]
-        else:
-            json=self.mejores_amarillo["0"]
-        
-        #llenar las acciones guardadas
-        for item in json:
-            col = item["columna"]
-            q  = item["q_value"]
-
-            self.Q_global[s0][col] = q
-            self.N_global[s0][col] = 1
-
-
-    def ucb(self, state_key, tablero): 
-        acciones=self.Q_global[state_key]
-        total= sum(self.N_global[state_key].values())
-        mejor=None
-        mejor_valor=-999
-
-        for a in acciones.keys():
-            if self.N_global[state_key][a]==0:
-                ucb=999
-            else:
-                q=self.Q_global[state_key][a]
-                n=self.N_global[state_key][a]
-                ucb= q + math.sqrt(math.log(total+1)/(n)) #formula del ucb
-
-            if ucb > mejor_valor:
-                mejor_valor=ucb
-                mejor=a
-        return mejor
-
-    def sim_random(self, tablero, color): #terminar la partida con jugadas random
+    def sim_random(self, tablero, color): #simulacion random hasta terminar
         s=ConnectState(tablero.copy(), player=color)
 
         while not s.is_final():
@@ -73,136 +34,154 @@ class NICOLAS(Policy):
                 s=s.transition(c)
             except:
                 reward=0
-                break 
-            
-        #sabwr quien gano
+                break
+
         ganador=s.get_winner()
-        if ganador== color:
-            reward = 1 #ganar
+
+        if ganador==color:
+            reward=1 #gano
         elif ganador==0:
-            reward = 0 #empatar
+            reward=0 #empato
         else:
-            reward = -1 #perder
-            
+            reward=-1 #perdio
+
         self.rollout_rewards.append(reward)
         return reward
+    
 
+    def mcts(self, tablero, color, t): #busqueda mcts
+        state_root=ConnectState(tablero.copy(), player=color)
+        root_key=self.serializar(state_root.board)
 
+        Q={}
+        N={}
 
-    def mcts(self, tablero, color):
+        def estado_inicial(state, key, es_raiz): #inicializar estado del arbol
+            Q[key]={}
+            N[key]={}
 
-        tablero_copia= tablero.copy()
-        state=ConnectState(tablero_copia, player=color)
+            libres=state.get_free_cols()
 
-        camino=[]
-        state_key= self.serializar(state.board)
+            if es_raiz:
+                if color==-1:
+                    json_val=self.mejores_rojo[str(t)] if str(t) in self.mejores_rojo else []
+                else:
+                    json_val=self.mejores_amarillo[str(t)] if str(t) in self.mejores_amarillo else []
 
-        #seleccion
-        while state_key in self.Q_global and len(self.Q_global[state_key])>0:
-            a=self.ucb(state_key, state.board)
-            camino.append((state_key, a))
+                usados=set()
 
-            state= state.transition(a)
+                for item in json_val:
+                    col=item["columna"]
+                    q=item["q_value"]
 
-            #cambiar de turno
-            #color=color*-1
-            state_key= self.serializar(state.board)
+                    if col in libres:
+                        Q[key][col]= q
+                        N[key][col]=1
+                        usados.add(col)
 
-            if state.is_final(): #acabar si ya es el estado final
-                break
-        
-        #expansion
-        if state_key not in self.Q_global:
-            self.Q_global[state_key]={}
-            self.N_global[state_key]={}
+                for col in libres:
+                    if col not in usados:
+                        Q[key][col]=0.0
+                        N[key][col]=0
 
-            for a in self.acciones_legales(state.board):
-                self.Q_global[state_key][a]=0
-                self.N_global[state_key][a]=0
-
-        #simulacion
-        reward= self.sim_random(state.board, state.player)
-
-        #backpropagation
-        for (s,a) in camino:
-            self.N_global[s][a] +=1
-            n= self.N_global[s][a]
-            qanterior= self.Q_global[s][a]
-            self.Q_global[s][a] = qanterior + (reward - qanterior)/n
-
-
-    def expandir_estado(self, tablero, state_key):
-        self.Q_global[state_key]={}
-        self.N_global[state_key]={}
-
-        for a in self.acciones_legales(tablero):
-            self.Q_global[state_key][a]=0
-            self.N_global[state_key][a]=0
-        
-        for i in range(60): #cuantas simulaciones se van a hacer para cada estado
-            self.mcts(tablero, self.mi_color)
-
-
-    #@override
-    def mount(self, time_out: int) -> None:
-        self.time_out=time_out
-        self.Q_global={}
-        self.N_global={}
-        
-        self.rollout_rewards=[] #para ver como van las simulaciones en las graficas
-
-        self.mi_color= None #mientras se sabe si es rojo o amarillo
-        self.estado_raiz_iniciado= False
-
-        #cargar los jsons
-        dir_path = os.path.dirname(__file__)  # carpeta donde está policy.py
-        rojo_path = os.path.join(dir_path, "mejores_rojo.json")
-        amarillo_path = os.path.join(dir_path, "mejores_amarillo.json")
-        
-        #json del rojo
-        with open(rojo_path, 'r') as f:
-            self.mejores_rojo = json.load(f)
-
-        #json del amarillo
-        with open(amarillo_path, 'r') as f:
-            self.mejores_amarillo = json.load(f)
-
-        
-
-    #@override
-    def act(self, s: np.ndarray) -> int:
-
-        if self.mi_color is None:
-            if np.count_nonzero(s == 1) == np.count_nonzero(s == -1):
-                self.mi_color = -1 #rojo
             else:
-                self.mi_color = 1 #amarillo
-        
-        #iniciar el estado incial con el color correcto
-        if not self.estado_raiz_iniciado:
-            self.estado_raiz()
-            self.estado_raiz_iniciado=True
+                for col in libres:
+                    Q[key][col]=0.0
+                    N[key][col]=0
 
-        #serializar el estado actual
-        state_key= self.serializar(s)
+        estado_inicial(state_root, root_key, True)
 
-        #si este no existe en el q global entonces se empieza a expandir a partir de ahi
-        if state_key not in self.Q_global:
-            self.expandir_estado(s, state_key)
+        #iteraciones
+        for _ in range(self.budget):
+            state=state_root
+            key=root_key
+            camino=[]
 
-        #elegir la mejor accion segun los q values
-        acciones=self.Q_global[state_key]
+            #seleccion
+            while True:
+                if state.is_final():
+                    break
 
-# si no hay q values para ese estado jugar random valido
-        if not acciones:
-            cols_libres = self.acciones_legales(s) 
-            return int(np.random.choice(cols_libres))
-        
-        mejor_qvalue=-999
-        mejor_accion=None
+                acciones=list(Q[key].keys())
+                total=sum(N[key].values())+1
 
-        for accion, q_value in acciones.items():
-            if q_value > mejor_qvalue:
-                mejor_qvalue=q_value
-                mejor_accion=accion
-        return mejor_accion
+                mejor=None
+                mejor_valor=-999999
+
+                for a in acciones:
+                    n=N[key][a]
+                    q=Q[key][a]
+
+                    if n==0:
+                        ucb=999999
+                    else:
+                        ucb=q+math.sqrt(math.log(total)/n)
+
+                    if ucb>mejor_valor:
+                        mejor_valor=ucb
+                        mejor=a
+
+                camino.append((key, mejor, state.player))
+                state=state.transition(mejor)
+
+                key=self.serializar(state.board)
+
+                if key not in Q:
+                    estado_inicial(state, key, False)
+                    break
+
+            #simulacion
+            reward=self.sim_random(state.board, color)
+
+            #backpropagation
+            for (k, a, jugador) in reversed(camino):
+                valor=reward
+
+                if jugador!=color:
+                    valor=-reward
+
+                N[k][a]+=1
+                n=N[k][a]
+                Q[k][a]+= (valor-Q[k][a])/n
+
+        #elegir la mejor accion
+        acciones=Q[root_key]
+        mejor_q=-999999
+        mejor_a=None
+
+        for a, q in acciones.items():
+            if q>mejor_q:
+                mejor_q=q
+                mejor_a=a
+
+        return int(mejor_a)
+    
+    #@override
+    def mount(self, time_out: int)->None: #cargar jsons de qvalues precargados y parametros
+        self.time_out=time_out
+        self.rollout_rewards=[] #para ver como van las simulaciones en las graficas
+        self.mi_color=None
+
+        dir_path=os.path.dirname(__file__)
+        rojo_path=os.path.join(dir_path, "mejores_rojo.json")
+        amarillo_path=os.path.join(dir_path, "mejores_amarillo.json")
+
+        with open(rojo_path, 'r') as f:
+            self.mejores_rojo=json.load(f)
+
+        with open(amarillo_path, 'r') as f:
+            self.mejores_amarillo=json.load(f)
+
+        self.budget=1000
+    
+    #@override
+    def act(self, s: np.ndarray)->int: #elige la jugada final
+        if self.mi_color is None:
+            if np.count_nonzero(s==1)==np.count_nonzero(s==-1):
+                self.mi_color=-1
+            else:
+                self.mi_color=1
+
+        t=int(np.count_nonzero(s))
+
+        return self.mcts(s, self.mi_color, t)
